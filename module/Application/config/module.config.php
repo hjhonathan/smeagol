1567f16b1f4d949bc426e2562bbf744e1f0c96fa
Herbert Chumpitaz <herbert.chumpitaz@gmail.com>
2014-07-04 09:52:36 -0500

    Actualizando smeagol

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -96,7 +96,17 @@ return array(
     'asset_manager' => array(
         'resolver_configs' => array(
             'paths' => array(
-                __DIR__ . '/../public',
+                __DIR__ . '/../public',    // public del module Application
+                __DIR__ . '/../../../themes/enterprise/css/images',
+            ),
+            // este mapeo puede ser dinamico desde base de datos o recorriendo el directorio
+            'map' => array(
+                'themes/enterprise/css/style.css' => __DIR__ . '/../../../themes/enterprise/css/style.css',
+                'themes/enterprise/css/ie6.css' => __DIR__ . '/../../../themes/enterprise/css/ie6.css',
+                'themes/enterprise/js/jquery-1.4.2.js' => __DIR__ . '/../../../themes/enterprise/js/jquery-1.4.2.js',
+                'themes/enterprise/js/jquery.jcarousel.js' => __DIR__ . '/../../../themes/enterprise/js/jquery.jcarousel.js',
+                'themes/enterprise/js/jquery.pngFix.js' => __DIR__ . '/../../../themes/enterprise/js/jquery.pngFix.js',
+                'themes/enterprise/js/js-fnc.js' => __DIR__ . '/../../../themes/enterprise/js/js-fnc.js',
             ),
         ),
         'caching' => array(
